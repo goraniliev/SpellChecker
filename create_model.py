e8b3405a9791b636f850e0a69a4dc569f042cf4a
Goran Iliev <goraniliev93@yahoo.com>
2016-08-30 23:30:33 +0200

    code refactoring

---
+++ create_model.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import time
+
+__author__ = 'goran'
+
+text_path = '../SpellCheckingData/dataset-hw.txt'
+separated_words_path = '../SpellCheckingData/dataset.txt'
+all_words_path = '../SpellCheckingData/all_words.txt'
+
+
+def extract(line):
+    word = []
+    words = []
+
+    for c in line:
+        if c.isalpha():
+            word.append(c)
+        elif word:
+            words.append(''.join(word).lower())
+            word = []
+
+    if word:
+        words.append(''.join(word).lower())
+
+    return words
+
+
+def train(features):
+    model = {}
+
+    for f in features:
+        model[f] = model.get(f, 1) + 1
+
+    return model
+
+
+def get_words(path=separated_words_path):
+    words = []
+    with open(path, 'r') as fin:
+        for line in fin:
+            category, content = line.split('\t')
+            words += content.split(' ')
+
+    return words
+
+
+def transform_dataset(path=text_path, res_path=separated_words_path):
+    # create empty file
+    open(res_path, 'w').close()
+
+    with open(path, 'r') as fin:
+        with open(res_path, 'a') as fout:
+            for line in fin:
+                try:
+                    category, content = line.decode('utf-8').split('\t')
+                except ValueError:
+                    print line
+                    break
+                # print type(content)
+                extracted = extract(content)
+                # print(' '.join(extract(content.decode('utf-8'))))
+                fout.write((category + '\t' + ' '.join(extracted) + '\n').encode('utf-8'))
+
+
+def all_words_toghether(path=all_words_path):
+    words = get_words()
+    with open(path, 'w') as fout:
+        fout.write(' '.join(words))
+
+
+def get_all_words(path=all_words_path):
+    with open(path, 'r') as fin:
+        words = fin.read().decode('utf-8').split(' ')
+    return words
+
+if __name__ == '__main__':
+    start = time.time()
+    transform_dataset()
+    end = time.time()
+
+    # 108.331707954 fout ako ja otvaram i zatvaram cesto
+    # 92.1931741238 fout ako ja otvoram porano
+    print end - start
+
+    all_words_toghether()
--- spell_checker.py
+++ spell_checker.py
@@ -1,85 +1,10 @@
 # -*- coding: utf-8 -*-
 import time
+from create_model import get_all_words, train
 
 __author__ = 'goran'
 
 # text_path = '/home/goran/Desktop/data/dataset-hw.txt'
-
-text_path = '../../Domasni/dataset-hw.txt'
-separated_words_path = '../dataset.txt'
-all_words_path = '../all_words.txt'
-
-def extract(line):
-    word = []
-    words = []
-
-    for c in line:
-        if c.isalpha():
-            word.append(c)
-        elif word:
-                words.append(''.join(word).lower())
-                word = []
-
-    if word:
-        words.append(''.join(word).lower())
-
-    return words
-
-
-def train(features):
-    model = {}
-
-    for f in features:
-        model[f] = model.get(f, 1) + 1
-
-    return model
-
-
-def get_words(path=separated_words_path):
-    words = []
-    with open(path, 'r') as fin:
-        for line in fin:
-            category, content = line.split('\t')
-            words += content.split(' ')
-
-    return words
-
-def transform_dataset(path=text_path, res_path=separated_words_path):
-    # create empty file
-    open(res_path, 'w').close()
-
-    with open(path, 'r') as fin:
-        with open(res_path, 'a') as fout:
-            for line in fin:
-                try:
-                    category, content = line.decode('utf-8').split('\t')
-                except ValueError:
-                    print line
-                    break
-                fout.write(category + '\t' + ' '.join(extract(content.decode('utf-8'))).encode('utf-8') + '\n')
-
-
-def all_words_toghether(path=all_words_path):
-    words = get_words()
-    with open(path, 'w') as fout:
-        fout.write(' '.join(words))
-
-
-def get_all_words(path=all_words_path):
-    with open(path, 'r') as fin:
-        words = fin.read().decode('utf-8').split(' ')
-    return words
-# start = time.time()
-# transform_dataset()
-# end = time.time()
-
-# 108.331707954 fout ako ja otvaram i zatvaram cesto
-# 92.1931741238 fout ako ja otvoram porano
-# print end - start
-
-# all_words_toghether()
-
-
 alphabet = 'abcdefgh'
 
 def levenshtein(x, y):
@@ -103,29 +28,32 @@ def levenshtein(x, y):
 
     return dp[M][N]
 
-s = time.time()
-words = get_all_words()
-e = time.time()
-print 'Words loaded from file in %f seconds' %(e - s)
-unique_words = set(words)
-s = time.time()
-model = train(words)
-e = time.time()
-print 'Model built in %f seconds' %(e - s)
-
-def edits(word):
+def edits(word, unique_words):
     suggestions = []
+    print 'Unique Words Count {}'.format(len(unique_words))
     for w in unique_words:
         if levenshtein(w, word) < 2:
             suggestions.append(w)
 
     return sorted(suggestions, key=lambda x: -model[x])
 
-s = time.time()
-suggestions = edits('чвек'.decode('utf-8'))
-for w in suggestions:
-    print w
-e = time.time()
-print 'Edit dist one word suggestions found in %f seconds' %(e - s)
+if __name__ == '__main__':
+    s = time.time()
+    words = get_all_words()
+    e = time.time()
+    print 'Words loaded from file in %f seconds' %(e - s)
+    unique_words = set(words)
+    s = time.time()
+    model = train(words)
+    e = time.time()
+    print 'Model built in %f seconds' %(e - s)
+
+    # Spell Checking
+    s = time.time()
+    suggestions = edits('чвек'.decode('utf-8'), unique_words)
+    for w in suggestions:
+        print w
+    e = time.time()
+    print 'Edit dist one word suggestions found in %f seconds' %(e - s)
 
 
